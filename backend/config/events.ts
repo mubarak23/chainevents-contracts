f9ea56f29c852acdfbf34ae653715a653d2abb48
Ikem <91676649+ikemHood@users.noreply.github.com>
2024-11-26 12:32:11 +0100

    Setup apibara for indexing Contract Events - Backend (#53)
    * implemented basic handlers and indexer
    
    * added migrations and updated models
    
    * handled emmited events

---
+++ backend/config/events.ts
@@ -0,0 +1,7 @@
+export const events = {
+    NewEventAdded: "NewEventAdded", //replace with event identify
+    RegisteredForEvent: "RegisteredForEvent",
+    EventAttendanceMark: "EventAttendanceMark",
+    EndEventRegistration: "EndEventRegistration",
+    RSVPForEvent: "RSVPForEvent"
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import { startIndexer } from "./indexer/index.js";
+
 dotenv.config();
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  startIndexer();
 });
