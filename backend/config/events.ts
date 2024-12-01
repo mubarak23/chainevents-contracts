5170aed9ecee840f02e90d536921d79582a6425a
Ikem Peter <ikempeter2020@gmail.com>
2024-12-01 15:20:20 +0100

    rm

--- backend/config/events.ts
+++
@@ -1,7 +0,0 @@
-export const events = {
-    NewEventAdded: "NewEventAdded", //replace with event identify
-    RegisteredForEvent: "RegisteredForEvent",
-    EventAttendanceMark: "EventAttendanceMark",
-    EndEventRegistration: "EndEventRegistration",
-    RSVPForEvent: "RSVPForEvent"
-};
